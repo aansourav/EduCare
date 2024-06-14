59d9eb6f76fdd82b669b65b83801a5e4f09b28ac
SoUrAv <aansourav@gmail.com>
2024-06-14 20:24:02 +0600

    Home page implemented

--- app/layout.js
+++ app/layout.js
@@ -3,18 +3,16 @@ import { connectDB } from "@/database/connection";
 import { cn } from "@/lib/utils";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
 const inter = Inter({ subsets: ["latin"] });
 const poppins = Inter({ subsets: ["latin"], variable: "--font-poppins" });
 
 export const metadata = {
-    title: "EduCare",
-    description: "Online Course Platform for Everyone 🚀",
+    title: "EduConnect - Wold's Best Learning Platform",
+    description: "Explore || Learn || Build || Share",
 };
 
 export default async function RootLayout({ children }) {
     const connection = await connectDB();
-    // console.log(connection);
     return (
         <html lang="en">
             <body className={cn(inter.className, poppins.className)}>
---
+++ database/queries/categories.js
@@ -0,0 +1,9 @@
+import { replaceMongoIdInArray } from "@/lib/convertData";
+import { connectDB } from "../connection";
+import { Category } from "../model/category-model";
+
+export async function getCategories() {
+    await connectDB();
+    const categories = await Category.find({}).lean();
+    return replaceMongoIdInArray(categories);
+}
