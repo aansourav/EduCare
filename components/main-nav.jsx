b7c49ca6eec3181930ae2857312f3d69e7da6265
SoUrAv <aansourav@gmail.com>
2024-06-26 23:32:37 +0600

    Nav menu login register implemented

--- components/main-nav.jsx
+++ components/main-nav.jsx
@@ -2,10 +2,11 @@
 
 import { cn } from "@/lib/utils";
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import { MobileNav } from "@/components/mobile-nav";
 import { Menu, X } from "lucide-react";
+import { signOut, useSession } from "next-auth/react";
 import { Logo } from "./logo";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { Button, buttonVariants } from "./ui/button";
@@ -17,7 +18,11 @@ import {
 } from "./ui/dropdown-menu";
 export function MainNav({ items, children }) {
     const [showMobileMenu, setShowMobileMenu] = useState(false);
-
+    const { data: session } = useSession();
+    const [loginSession, setLoginSession] = useState(null);
+    useEffect(() => {
+        setLoginSession(session);
+    }, [session]);
     return (
         <>
             <div className="flex gap-6 lg:gap-10">
@@ -46,31 +51,41 @@ export function MainNav({ items, children }) {
                 )}
             </div>
             <nav className="flex items-center gap-3">
-                <div className="items-center gap-3 hidden lg:flex">
-                    <Link
-                        href="/login"
-                        className={cn(buttonVariants({ size: "sm" }), "px-4")}
-                    >
-                        Login
-                    </Link>
-                    <DropdownMenu>
-                        <DropdownMenuTrigger asChild>
-                            <Button variant="outline" size="sm">
-                                Register
-                            </Button>
-                        </DropdownMenuTrigger>
-                        <DropdownMenuContent align="end" className="w-56 mt-4">
-                            <DropdownMenuItem className="cursor-pointer">
-                                <Link href="/register/student">Student</Link>
-                            </DropdownMenuItem>
-                            <DropdownMenuItem className="cursor-pointer">
-                                <Link href="/register/instructor">
-                                    Instructor
-                                </Link>
-                            </DropdownMenuItem>
-                        </DropdownMenuContent>
-                    </DropdownMenu>
-                </div>
+                {!loginSession && (
+                    <div className="items-center gap-3 hidden lg:flex">
+                        <Link
+                            href="/login"
+                            className={cn(
+                                buttonVariants({ size: "sm" }),
+                                "px-4"
+                            )}
+                        >
+                            Login
+                        </Link>
+                        <DropdownMenu>
+                            <DropdownMenuTrigger asChild>
+                                <Button variant="outline" size="sm">
+                                    Register
+                                </Button>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent
+                                align="end"
+                                className="w-56 mt-4"
+                            >
+                                <DropdownMenuItem className="cursor-pointer">
+                                    <Link href="/register/student">
+                                        Student
+                                    </Link>
+                                </DropdownMenuItem>
+                                <DropdownMenuItem className="cursor-pointer">
+                                    <Link href="/register/instructor">
+                                        Instructor
+                                    </Link>
+                                </DropdownMenuItem>
+                            </DropdownMenuContent>
+                        </DropdownMenu>
+                    </div>
+                )}
                 <DropdownMenu>
                     <DropdownMenuTrigger asChild>
                         <div className="cursor-pointer">
@@ -96,7 +111,9 @@ export function MainNav({ items, children }) {
                             <Link href="">Testimonials & Certificates</Link>
                         </DropdownMenuItem>
                         <DropdownMenuItem className="cursor-pointer" asChild>
-                            <Link href="">Logout</Link>
+                            <Link href="#" onClick={() => signOut()}>
+                                Logout
+                            </Link>
                         </DropdownMenuItem>
                     </DropdownMenuContent>
                 </DropdownMenu>
